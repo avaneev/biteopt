03f78c50a8d742e212f6f7d6776f3d2d64f9d2ef
Aleksey Vaneev <aleksey.vaneev@gmail.com>
2018-09-29 08:31:51 +0300

    Updated to demonstrate larger prime numbers factoring.

--- primefactor.cpp
+++ primefactor.cpp
@@ -6,10 +6,10 @@
 #include <stdint.h>
 #include "biteopt.h"
 
-const int N = 16;
-double lb[ N ] = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
-double ub[ N ] = { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 };
-int64_t val = 20000003LL * 10000079LL;
+const int N = 8;
+double lb[ N ] = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+double ub[ N ] = { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 };
+int64_t val = 160000003LL * 10000079LL;
 bool DoPrint = false;
 
 double fn( int ND, const double* x, void* data )
@@ -19,10 +19,10 @@ double fn( int ND, const double* x, void* data )
 	double cost = 0.0;
 	int i;
 
-	for( i = 0; i < 8; i++ )
+	for( i = 0; i < 4; i++ )
 	{
-		a1 += (int) ( x[ i ] * 25000000.0 + 0.5 );
-		a2 += (int) ( x[ 8 + i ] * 25000000.0 + 0.5 );
+		a1 += (int) ( x[ i ] * 50000000.0 + 0.5 );
+		a2 += (int) ( x[ 4 + i ] * 5000000.0 + 0.5 );
 	}
 
 	if(( a1 & 1 ) == 0 )
