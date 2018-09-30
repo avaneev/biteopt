fc9afa50cf19352eeb92d84e0b07f560dcbb0654
Aleksey Vaneev <aleksey.vaneev@gmail.com>
2018-09-30 10:35:50 +0300

    Reimplemented biprime factoring method, works much faster now.

--- primefactor.cpp
+++ primefactor.cpp
@@ -1,78 +1,122 @@
-// Prime-number factoring example. Uses BiteOptDeep with a huge (5000) depth
-// factor. This is purely a proof-of-concept example, may not work for
-// factoring of huge numbers, so far works for 17-digit numbers. Since this
-// method is probabilistic, various variables should be changed to obtain
-// solutions - iteration number, depth, attempt count, MinMaxFactor values,
-// Split. The resulting "minf" is equal to 0 when a precise solution was
+// Biprime-number factoring method. Uses BiteOptDeep with a huge depth factor.
+// This is purely a proof-of-concept example, may not work for factoring of
+// huge numbers, so far works for 19-digit numbers, does not work for all
+// numbers. Since this method is probabilistic, various variables should be
+// changed to obtain solutions: depth, attempt count, MaxFactor/MaxDiff
+// values. The resulting "cost" is equal to 0 when a precise solution was
 // found.
 //
-// Currently method uses just an even-odd check as a primality test. Using a
-// more elaborative and precise test may produce better convergence property.
+// This method is highly-parallelizable, each attempt can be executed in
+// parallel in its own thread. Tests indicated that this method finds factors
+// extremely fast when factors have similar magnitude.
 
 #include <stdio.h>
 #include <stdint.h>
 #include "biteopt.h"
 
-const int64_t val = 160000003LL * 148030163LL; // n=pq
-const int Split = 4;
-const int N = Split * 2;
-const double lb[] = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
-const double ub[] = { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 };
-const double MinFactor1 = 0.0;
-const double MaxFactor1 = 400000000.0;
-const double Factor1Diff = ( MaxFactor1 - MinFactor1 ) / Split;
-const double MinFactor2 = 0.0;
-const double MaxFactor2 = 400000000.0;
-const double Factor2Diff = ( MaxFactor2 - MinFactor2 ) / Split;
+const uint64_t val = 1012322327LL * 1115382761LL; //322532699LL * 512532767LL;//7152347LL * 3152399LL;//n=pq
+const int Depth = 100; // BiteOptDeep depth factor, affects lower bound of
+	// achieved cost function. Possibly should be increased if a larger number
+	// have to be factored.
+const int Attempts = 200000; // Factorization attempts to perform.
+const int MaxIters = (int) ( 2000000.0 * sqrt( (double) Depth )); // Maximal
+	// number of iterations to perform.
+const int StallFactor = 50; // Factor to stop attempt at when optimizer stalls.
+const int N = 2; // The number of dimensions in optimizer.
+const double MaxFactor = 2000000000.0 / 2; // Max value of factors.
+const double MaxDiff = 300000000.0 / 2; // Max difference between factors.
 bool DoPrint = false;
 
-double fn( int ND, const double* x, void* data )
+/**
+ * Optimizer class.
+ */
+
+class CPrimeFactorizerOpt : public CBiteOptDeep
 {
-	int64_t a1 = 0;
-	int64_t a2 = 0;
-	double cost = 0.0;
-	int i;
-
-	for( i = 0; i < Split; i++ )
+public:
+	CPrimeFactorizerOpt()
 	{
-		a1 += (int64_t) ( MinFactor1 + x[ i ] * Factor1Diff + 0.5 );
-		a2 += (int64_t) ( MinFactor2 + x[ Split + i ] * Factor2Diff + 0.5 );
+		updateDims( N, Depth );
 	}
 
-	if(( a1 & 1 ) == 0 )
+	virtual void getMinValues( double* const p ) const
 	{
-		cost += 1e10;
+		int i;
+
+		for( i = 0; i < N; i++ )
+		{
+			p[ i ] = 0.0;
+		}
 	}
 
-	if(( a2 & 1 ) == 0 )
+	virtual void getMaxValues( double* const p ) const
 	{
-		cost += 1e10;
+		int i;
+
+		for( i = 0; i < N; i++ )
+		{
+			p[ i ] = 1.0;
+		}
 	}
 
-	int64_t d = val - a1 * a2;
-
-	if( d < 0 )
+	virtual double optcost( const double* const p )
 	{
-		d = -d;
-	}
+		uint64_t a1 = (uint64_t) ( p[ 0 ] * MaxFactor );
+		uint64_t a2 = a1 + (uint64_t) ( p[ 1 ] * MaxDiff );
 
-	if( DoPrint )
-	{
-		printf( "%lli\n", a1 );
-		printf( "%lli\n", a2 );
-	}
+		a1 = ( a1 << 1 ) | 1; // Produce odd factor.
+		a2 = ( a2 << 1 ) | 1; // Produce odd factor.
 
-	return( d + cost );
-}
+		const uint64_t nval = a1 * a2;
+		const uint64_t d = ( val > nval ? val - nval : nval - val );
+
+		if( DoPrint )
+		{
+			printf( "    f1 = %llu\n", a1 );
+			printf( "    f2 = %llu\n", a2 );
+			printf( "n = pq = %llu\n", val );
+			printf( " f1*f2 = %llu\n", nval );
+		}
+
+		return( (double) d );
+	}
+};
 
 int main()
 {
-	double x[ N ];
-	double minf;
-	biteopt_minimize( N, fn, NULL, lb, ub, x, &minf, 2000000, 5000, 10 );
+	int j;
 
-	printf( "minf = %.10g\n", minf );
+	for( j = 0; j < Attempts; j++ )
+	{
+		CBiteRnd rnd;
+		rnd.init( j + 1 );
 
-	DoPrint = true;
-	fn( N, x, NULL );
+		CPrimeFactorizerOpt opt;
+		opt.init( rnd );
+		const int MaxStallCount = opt.getInitEvals() / Depth * StallFactor;
+
+		int i;
+
+		for( i = 0; i < MaxIters; i++ )
+		{
+			if( opt.optimize( rnd ) > MaxStallCount )
+			{
+				// Optimization stalled.
+
+				break;
+			}
+		}
+
+		printf( "attempt\t%i\tcost\t%.8g\titers\t%i\n", j + 1,
+			opt.getBestCost(), i );
+
+		if( opt.getBestCost() == 0.0 )
+		{
+			printf( "Solution found\n" );
+
+			DoPrint = true;
+			opt.optcost( opt.getBestParams() );
+			break;
+		}
+	}
 }
